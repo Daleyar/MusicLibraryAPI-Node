f105364e4a53ac4992461702c93cd5b3eadab103
Ali Daley <ali.daley72@gmail.com>
2021-11-30 16:55:28 -0500

    Created GET endpoint to get songs by ID

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.listen(3000, function() {
 });
 
 app.get('/api/songs',(req,res)=>{
-    const songs=repoContext.songs.findAllSongs();
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+app.get('/api/songs/:id',(req,res)=>{
+    const id = req.params.id;
+    const songs = repoContext.songs.findSongById(id);
     return res.send(songs);
 });
